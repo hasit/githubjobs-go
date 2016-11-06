f3a565b2a998c3dedd7890a821453c6f941cb165
Hasit Mistry <hasitnm@gmail.com>
2016-11-05 18:53:33 -0700

    Major changes
    - Perform the action of getting response in a separate function
    - Add Position method for pretty printing struct

--- githubjobs.go
+++ githubjobs.go
@@ -1,9 +1,12 @@
 package githubjobs
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"reflect"
 
 	"github.com/google/go-querystring/query"
 )
@@ -47,8 +50,12 @@ func (e Error) Error() string {
 	return fmt.Sprintf("githubjobs: %s (%d)", e.Message, e.Code)
 }
 
+func (p Position) String() string {
+	return stringify(p)
+}
+
 // GetPositions gets positions from Github Jobs by description and location.
-func GetPositions(description, location string, fullTime bool) ([]Position, error) {
+func GetPositions(description, location string, fullTime bool) ([]*Position, error) {
 	sq := searchQueries{
 		Description: description,
 		Location:    location,
@@ -56,57 +63,153 @@ func GetPositions(description, location string, fullTime bool) ([]Position, erro
 	}
 	v, _ := query.Values(sq)
 
-	var p *[]Position
-
 	url := fmt.Sprintf("%v", endpoint+"/positions.json?"+v.Encode())
 
-	r, err := http.Get(url)
+	positions := new([]*Position)
+
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return *p, Error{fmt.Sprintf("Could not create request: %s", err), -1}
+		return nil, Error{fmt.Sprintf("Could not create request: %s", err), -1}
 	}
-	defer r.Body.Close()
+	req.URL.RawQuery = v.Encode()
 
-	json.NewDecoder(r.Body).Decode(&p)
+	err = clientRequest(req, positions)
+	if err != nil {
+		return nil, err
+	}
 
-	return *p, nil
+	return *positions, nil
 }
 
 // GetPositionsByCoordinates gets positions from Github Jobs by coordinates (latitude and longitude) in decimal degrees.
-func GetPositionsByCoordinates(latitude, longitude string) ([]Position, error) {
+func GetPositionsByCoordinates(latitude, longitude string) ([]*Position, error) {
 	cq := cooridinateQueries{
 		Latitude:  latitude,
 		Longitude: longitude,
 	}
 	v, _ := query.Values(cq)
 
-	url := fmt.Sprintf("%v", endpoint+"/positions.json?"+v.Encode())
+	url := fmt.Sprintf("%v", endpoint+"/positions.json?")
 
-	var p *[]Position
+	positions := new([]*Position)
 
-	r, err := http.Get(url)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return *p, Error{fmt.Sprintf("Could not create request: %s", err), -1}
+		return nil, Error{fmt.Sprintf("Could not create request: %s", err), -1}
 	}
-	defer r.Body.Close()
+	req.URL.RawQuery = v.Encode()
 
-	json.NewDecoder(r.Body).Decode(&p)
+	err = clientRequest(req, positions)
+	if err != nil {
+		return nil, err
+	}
 
-	return *p, nil
+	return *positions, nil
 }
 
 // GetPositionByID gets a single job posting from Github Jobs by ID.
-func GetPositionByID(ID string) (Position, error) {
+func GetPositionByID(ID string) (*Position, error) {
 	url := fmt.Sprintf("%v", endpoint+"/positions/"+ID+".json")
 
-	var p *Position
+	position := new(Position)
 
-	r, err := http.Get(url)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return *p, Error{fmt.Sprintf("Could not create request: %s", err), -1}
+		return nil, Error{fmt.Sprintf("Could not create request: %s", err), -1}
 	}
-	defer r.Body.Close()
 
-	json.NewDecoder(r.Body).Decode(&p)
+	err = clientRequest(req, position)
+	if err != nil {
+		return nil, err
+	}
 
-	return *p, nil
+	return position, nil
+}
+
+func clientRequest(req *http.Request, v interface{}) error {
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return Error{fmt.Sprintf("Failed to make request: %s", err), -1}
+	}
+	defer res.Body.Close()
+
+	err = json.NewDecoder(res.Body).Decode(v)
+	if err != nil {
+		return Error{fmt.Sprintf("Could not read JSON response: %s", err), -1}
+	}
+	if err == io.EOF {
+		err = nil
+	}
+	return err
+}
+
+// Stringify attempts to create a reasonable string representation of types in.
+// It does things like resolve pointers to their values and omits struct fields with nil values.
+func stringify(message interface{}) string {
+	var buf bytes.Buffer
+	v := reflect.ValueOf(message)
+	stringifyValue(&buf, v)
+	return buf.String()
+}
+
+// stringifyValue was adopted from go-github library https://github.com/google/go-github/blob/master/github/strings.go.
+func stringifyValue(w io.Writer, val reflect.Value) {
+	if val.Kind() == reflect.Ptr && val.IsNil() {
+		w.Write([]byte("<nil>"))
+		return
+	}
+
+	v := reflect.Indirect(val)
+
+	switch v.Kind() {
+	case reflect.String:
+		fmt.Fprintf(w, `"%s"`, v)
+	case reflect.Slice:
+		w.Write([]byte{'['})
+		for i := 0; i < v.Len(); i++ {
+			if i > 0 {
+				w.Write([]byte{' '})
+			}
+
+			stringifyValue(w, v.Index(i))
+		}
+
+		w.Write([]byte{']'})
+		return
+	case reflect.Struct:
+		if v.Type().Name() != "" {
+			w.Write([]byte(v.Type().String()))
+		}
+
+		w.Write([]byte{'{'})
+
+		var sep bool
+		for i := 0; i < v.NumField(); i++ {
+			fv := v.Field(i)
+			if fv.Kind() == reflect.Ptr && fv.IsNil() {
+				continue
+			}
+			if fv.Kind() == reflect.Slice && fv.IsNil() {
+				continue
+			}
+
+			if sep {
+				w.Write([]byte(", "))
+			} else {
+				sep = true
+			}
+
+			w.Write([]byte(v.Type().Field(i).Name))
+			w.Write([]byte{':'})
+			stringifyValue(w, fv)
+		}
+
+		w.Write([]byte{'}'})
+	default:
+		if v.CanInterface() {
+			fmt.Fprint(w, v.Interface())
+		}
+	}
 }
